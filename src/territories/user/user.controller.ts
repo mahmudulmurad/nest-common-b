7dc3654edfdd3739033d7d041b1ae0f689aed926
mahmudulmurad <mahmudul.murad@simecinstitute.edu.bd>
2026-04-16 17:31:27 +0600

    added watch api

--- src/territories/user/user.controller.ts
+++ src/territories/user/user.controller.ts
@@ -7,6 +7,9 @@ import {
   UploadedFile,
   Req,
   UseGuards,
+  Get,
+  Res,
+  StreamableFile,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignUpDto } from '../../dto/singup.dto';
@@ -18,6 +21,9 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadFileInterceptorMemory } from 'src/custom-interceptor/fileUpload.interceptor';
 import { CustomRequest } from 'src/interface/customRequest.interface';
 import { AuthGuard } from 'src/auth';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Request, Response } from 'express';
 
 @ApiTags('USER')
 @Controller('users')
@@ -69,5 +75,37 @@ export class UserController {
     } catch (error) {
       console.error('Error sending message to loggerService:', error);
     }
+  }
+
+  @Get('/watch')
+  watch(@Req() req: Request, @Res() res: Response) {
+    const videoPath = path.join(__dirname, '..', '..', '..', 'file.mp4');
+
+    const stat = fs.statSync(videoPath);
+    const fileSize = stat.size;
+
+    const range = req.headers.range;
+
+    if (!range) {
+      // First request – browser expects range
+      res.status(416).send('Range header required');
+      return;
+    }
+
+    const chunkSize = 1 * 1e6; // 1MB
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, fileSize - 1);
+
+    const contentLength = end - start + 1;
+
+    res.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': contentLength,
+      'Content-Type': 'video/mp4',
+    });
+
+    const stream = fs.createReadStream(videoPath, { start, end });
+    stream.pipe(res);
   }
 }
