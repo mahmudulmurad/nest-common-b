da2bf5597ce07a29362baa9c48cff55d4a549e30
mahmudulmurad <mahmudul.murad@simecinstitute.edu.bd>
2026-04-21 15:37:31 +0600

    remove security key for ec2 since using AmazoneS3FullAccess  roles directly from ec2 instance

--- src/custom-interceptor/s3FileService.ts
+++ src/custom-interceptor/s3FileService.ts
@@ -14,10 +14,6 @@ export class S3Service {
   constructor() {
     this.s3Client = new S3Client({
       region: process.env.AWS_REGION,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-      },
     });
   }
 
